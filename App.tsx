2b81f37e3740ad6c39b4637c9da8c4ff3c8ae790
Matias <mati_d_pinto@hotmail.com>
2023-10-10 13:30:26 -0300

    splash screen added & keystore

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 import {Provider} from 'react-redux';
 
 import {Router} from '@src/navigation';
 import {store} from '@src/redux/store';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
